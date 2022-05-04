31a713e211091b77b0d8a8a858c880828a9e485b
corey.stone001 <corey.stone001@gmail.com>
2022-05-04 08:32:12 -0700

    remove the import statement from app.js as this was blocking tinyslider from working; setup styles for slider partial and import into main.scss;

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,38 @@
-import { tns } from "tiny-slider";
+// yourScript.js
+// import { tns } from "./node_modules/tiny-slider/src/tiny-slider"
 
 let slider = tns({
-    "items": 3,
-    "slideBy": 1,
-    "controls": false,
-    "autoplay": true,
-    "responsive": {
-        "280": {
-        "items": 1,
-        "controls": true,
-        "edgePadding": 30
+    container: ".slider",
+    "slideBy": "1",
+    "speed": 400,
+    "nav" : false,
+    autoplay : true,
+    controls: false,
+    autoplayButtonOutput : false,
+    responsive: {
+        279:    {
+            items: 1,
+            gutter: 10,
         },
-        "500": {
-        "items": 4
+        500:    {
+            items: 2,
+            gutter: 20
         }
-    },
-    "container": "#responsive",
-    "swipeAngle": false,
-    "speed": 400
+    }
 })
+
+// $(document).ready(function () {
+//     let interval = window.setInterval(rotateSlides, 3000)
+
+//     function rotateSlides(){
+//         let $firstSlide = $('.slider').find('div:first');
+//         let width = $firstSlide.width();
+
+//         $firstSlide.animate({marginLeft: -width}, 1000, function(){
+//             let $lastSlide = $('.slider').find('div:last')
+//             $lastSlide.after($firstSlide);
+//             $firstSlide.css({marginLeft: 0})
+//         })
+//     }
+// });
+
